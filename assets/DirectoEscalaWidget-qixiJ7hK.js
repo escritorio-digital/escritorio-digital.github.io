e1c2de5e39a8b03e69411b264d947545aa290b73
Juan Jose de Haro <jjdeharo@gmail.com>
2026-01-14 09:45:10 +0100

    Updates

--- assets/DirectoEscalaWidget-qixiJ7hK.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DohbhN-v.js";import{g as l}from"./index-DohbhN-v.js";import{ExternalAppWidget as e}from"./ExternalAppWidget-BMSytzRS.js";const r=()=>t.jsx(e,{url:"https://jjdeharo.github.io/directo/escala.html",titleKey:"widgets.directo_escala.title"});export{r as DirectoEscalaWidget,l as widgetConfig};
---
+++ assets/DirectoMuroWidget-DiqDoQ8c.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C11GZ-IO.js";import{i as s}from"./index-C11GZ-IO.js";import{ExternalAppWidget as o}from"./ExternalAppWidget-DU1RAm2y.js";const e=()=>t.jsx(o,{url:"https://jjdeharo.github.io/directo/muro.html",titleKey:"widgets.directo_muro.title"});export{e as DirectoMuroWidget,s as widgetConfig};
