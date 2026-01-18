99456635217ad06bf93df734ef3b8f44918fd01c
Juan Jose de Haro <jjdeharo@gmail.com>
2026-01-18 12:34:22 +0100

    Updates

--- assets/vendor-tiptap-extension-document-DmBpm_Dh.js
+++
@@ -1,1 +0,0 @@
-import{N as o}from"./vendor-tiptap-core-CKFakkLY.js";var t=o.create({name:"doc",topNode:!0,content:"block+"});export{t as D};
--- assets/vendor-tiptap-extension-text-BVHjtOnu.js
+++
@@ -1,1 +0,0 @@
-import{N as e}from"./vendor-tiptap-core-CKFakkLY.js";var t=e.create({name:"text",group:"inline"});export{t as T};
