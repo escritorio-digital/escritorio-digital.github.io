00d77a18f53104ab79058f7f27cc8d2be519538a
Juan Jose de Haro <jjdeharo@gmail.com>
2026-03-12 08:46:13 +0100

    Updates

---
+++ assets/DirectoEscalaWidget-Cd79IYJs.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DluoYVAn.js";import{p as s}from"./index-DluoYVAn.js";import{ExternalAppWidget as e}from"./ExternalAppWidget-BNflucSK.js";const a=()=>t.jsx(e,{url:"https://jjdeharo.github.io/directo/escala.html",titleKey:"widgets.directo_escala.title",appendLanguageParam:!0,openInPopup:!0});export{a as DirectoEscalaWidget,s as widgetConfig};
---
+++ assets/DirectoVotaWidget-CAlA-HHG.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DluoYVAn.js";import{y as d}from"./index-DluoYVAn.js";import{ExternalAppWidget as e}from"./ExternalAppWidget-BNflucSK.js";const i=()=>t.jsx(e,{url:"https://jjdeharo.github.io/directo/vota.html",titleKey:"widgets.directo_vota.title",appendLanguageParam:!0,openInPopup:!0});export{i as DirectoVotaWidget,d as widgetConfig};
