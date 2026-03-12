867f1a2a35986254121beb33c6350dc677878b08
Juan Jose de Haro <jjdeharo@gmail.com>
2026-03-12 06:00:16 +0100

    Updates

---
+++ assets/DirectoTicketWidget-EOTvaUrK.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DZcvo1C2.js";import{x as d}from"./index-DZcvo1C2.js";import{ExternalAppWidget as e}from"./ExternalAppWidget-DvbIjBuA.js";const r=()=>t.jsx(e,{url:"https://jjdeharo.github.io/directo/ticket.html",titleKey:"widgets.directo_ticket.title",appendLanguageParam:!0,openInPopup:!0});export{r as DirectoTicketWidget,d as widgetConfig};
