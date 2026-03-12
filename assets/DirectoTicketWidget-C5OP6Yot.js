69b2ea5cf3a07dcc07657380577e090504166804
Juan Jose de Haro <jjdeharo@gmail.com>
2026-03-12 10:32:24 +0100

    Updates

---
+++ assets/DirectoTicketWidget-C5OP6Yot.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C3VwQPJu.js";import{x as d}from"./index-C3VwQPJu.js";import{ExternalAppWidget as e}from"./ExternalAppWidget-C6s2gFMW.js";const r=()=>t.jsx(e,{url:"https://jjdeharo.github.io/directo/ticket.html",titleKey:"widgets.directo_ticket.title",appendLanguageParam:!0,openInPopup:!0});export{r as DirectoTicketWidget,d as widgetConfig};
--- assets/QPlayWidget-DYDe4dFr.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DluoYVAn.js";import{av as g}from"./index-DluoYVAn.js";import{ExternalAppWidget as e}from"./ExternalAppWidget-BNflucSK.js";const r=()=>t.jsx(e,{url:"https://jjdeharo.github.io/qplay/",titleKey:"widgets.qplay.title",appendLanguageParam:!0,openInPopup:!0});export{r as QPlayWidget,g as widgetConfig};
